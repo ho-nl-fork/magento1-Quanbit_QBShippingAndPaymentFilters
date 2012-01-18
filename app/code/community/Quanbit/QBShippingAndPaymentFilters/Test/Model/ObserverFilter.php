b6bf1ed8ffca2e8b02881ed1c98e4765f22a1c8a
alexsiri7 <alexsiri7@gmail.com>
2012-01-18 14:24:52 -0300

    fixed problem with installer

---
+++ app/code/community/Quanbit/QBShippingAndPaymentFilters/Test/Model/ObserverFilter.php
@@ -0,0 +1,63 @@
+<?php
+
+class Quanbit_QBShippingAndPaymentFilters_Test_Model_ObserverFilter extends EcomDev_PHPUnit_Test_Case{
+
+  public function setUp(){
+      parent::setUp();
+      $this->observer = Mage::getModel('checkoutrule/observer_filter');
+  }
+  public function getQuoteFor($quote_id){
+      $q = Mage::getModel('sales/quote');
+      $q->setStoreId(1);
+      $q->load($quote_id);
+      return $q;
+  }
+  private function getPaymentModel($payment_code){
+      return Mage::getModel(Mage::getStoreConfig('payment/'.$payment_code."/model"));
+  }
+  
+  /**
+   * @test
+   * @loadFixture base_filter
+   */
+    
+  public function testShouldCheck(){
+          
+     $args = array(
+            'quote'           => Mage::getModel('sales/order'),
+        );
+     $this->assertFalse($this->observer->shouldCheck(new Varien_Event()));
+     $this->assertTrue($this->observer->shouldCheck(new Varien_Event($args)));
+  }
+  /**
+   * @test
+   * @loadFixture base_filter
+   * @dataProvider dataProvider
+   * @loadExpectation 
+   */
+    
+  public function getRulesFor($method, $action, $method_type){
+     $rules = $this->observer->getRulesFor(1, $method, $action, $method_type);
+     $rule = $rules->getFirstItem();
+     $this->assertEquals($this->expected('%s-%s-%s', $method, $action, $method_type)->getRuleName(), $rule->getName());
+  }
+  /**
+   * @test
+   * @loadFixture base_filter
+   * @dataProvider dataProvider
+   */
+
+  public function rulesMatch ($rule_id, $quote_id, $result){
+      $rule = Mage::getModel('checkoutrule/rule')->load($rule_id);
+      $quote = $this->getQuoteFor($quote_id);
+      $args = array(
+            'quote'           => $quote,
+        );
+      
+      $event  = new Varien_Event($args);
+      $match = $this->observer->rulesMatch(array($rule), $quote);
+      $this->assertEquals($result, $match);
+  }
+
+}
+?>
